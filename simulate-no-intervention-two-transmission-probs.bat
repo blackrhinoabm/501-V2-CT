6055dd1756a766b57d44a3589288471c5054387f
joerischasfoort <joerischasfoort@gmail.com>
2021-01-21 08:15:02 +0100

    update with ODE notebook

--- simulate-no-intervention-two-transmission-probs.bat
+++ simulate-no-intervention-two-transmission-probs.bat
@@ -1,5 +1,5 @@
-FOR %%G IN ("first_strain", "second_strain") DO (
-  FOR /L %%A IN (0,1, 2) DO (
+FOR %%G IN ("second_strain") DO (
+  FOR /L %%A IN (0,1) DO (
     sabcom simulate -i cape_town -o output_data/%%G -s %%A -d csv-light -sc no-intervention -scf cape_town/%%Gparameters.json -init networks_for_first_wave
   )
 )
